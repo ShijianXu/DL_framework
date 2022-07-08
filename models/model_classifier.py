05e7fb68aa63c8e2cba4e9ce10afe8f90e5aad50
shijianxu <13260906215@163.com>
2022-07-08 14:12:59 +0200

    test model with config

---
+++ models/model_classifier.py
@@ -0,0 +1,30 @@
+import importlib
+
+import torch
+import torch.nn as nn
+
+class CLS_Model(nn.Module):
+    def __init__(self,
+        backbone,
+        backend,
+    ):
+        super(CLS_Model, self).__init__()
+
+        # build modules
+        self.backbone = CLS_Model.build_module(backbone)
+        self.backend = CLS_Model.build_module(backend)
+
+    @staticmethod
+    def build_module(module_config):
+        class_name = module_config["name"].split('.')[-1]
+        module_name = module_config["name"][:(len(module_config["name"]))-len(class_name)-1]
+        
+        class_type = getattr(importlib.import_module(module_name), class_name)
+        obj = class_type(**module_config["config"])
+
+        return obj
+
+    def forward(self, x):
+        x = self.backbone(x)
+        out = self.backend(x)
+        return out
--- train.py
+++ train.py
@@ -1,8 +1,6 @@
-import os
 import argparse
 import importlib
 
-from numpy import require
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Deep Learning Framework", formatter_class=argparse.RawTextHelpFormatter)
@@ -10,7 +8,7 @@ if __name__ == '__main__':
         "--config", required=True, type=str, help="The config file."
     )
     parser.add_argument(
-        "--log", required=True, type=str, help="Path to log."
+        "--log", type=str, help="Path to log."
     )
 
     args = parser.parse_args()
@@ -22,5 +20,4 @@ if __name__ == '__main__':
         config = importlib.util.module_from_spec(spec)
         exec(config_src, config.__dict__)
 
-    print(config.model)
-
+    # print(config.model)
