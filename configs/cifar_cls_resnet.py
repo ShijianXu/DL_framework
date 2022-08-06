1bba96ea55efb0ee29180953633d3982706d3867
shijianxu <13260906215@163.com>
2022-08-06 22:42:13 +0200

    data transform augmentation

--- configs/cifar_cls_resnet.py
+++ configs/cifar_cls_resnet.py
@@ -24,12 +24,19 @@ model = models.model_classifier.CLS_Model(**model_config)
 print(f"Total model parameters: {model.get_num_params()}")
 
 # Data part
-transform=transforms.Compose([
+# transform=transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+# ])
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=transform)
+train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=transform_train)
 train_dataloader = DataLoader(
     train_dataset, 
     batch_size=16, 
@@ -38,7 +45,13 @@ train_dataloader = DataLoader(
 )
 print("Construct train dataset with {} samples".format(len(train_dataset)))
 
-test_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=transform)
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+test_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=transform_test)
 test_dataloader = DataLoader(
     test_dataset, 
     batch_size=4, 
