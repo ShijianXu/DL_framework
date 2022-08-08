90dc3dcdf6a559e11041d15d302bb44c7ffec00d
shijianxu <13260906215@163.com>
2022-08-08 21:12:33 +0200

    add abstrcut model class

---
+++ models/__init__.py
@@ -0,0 +1,57 @@
+import importlib
+import torch.nn as nn
+
+class Abstract_Model(nn.Module):
+    def __init__(self,
+        backbone,
+        backend,
+        backbone_created=False,
+        backend_created=False
+    ):
+        super(Abstract_Model, self).__init__()
+
+        # build modules
+        if not backbone_created:
+            self.backbone = Abstract_Model.build_module(backbone)
+        else:
+            self.backbone = backbone
+
+        if not backend_created:
+            self.backend = Abstract_Model.build_module(backend)
+        else:
+            self.backend = backend
+
+        self.num_params_backbone = sum(param.numel() for param in self.backbone.parameters())
+        self.num_params_backend = sum(param.numel() for param in self.backend.parameters())
+
+    @staticmethod
+    def build_module(module_config):
+        class_name = module_config["name"].split('.')[-1]
+        module_name = module_config["name"][:(len(module_config["name"]))-len(class_name)-1]
+        
+        class_type = getattr(importlib.import_module(module_name), class_name)
+        obj = class_type(**module_config["config"])
+
+        return obj
+
+    def forward(self, x):
+        x = self.backbone(x)
+        out = self.backend(x)
+        return out
+
+    def get_num_params(self):
+        print(f"Backbone num of params: {self.num_params_backbone}")
+        print(f"Backend num of params: {self.num_params_backend}")
+        return self.num_params_backbone + self.num_params_backend
+
+    def compute_metric(self, output, target):
+        raise NotImplementedError("Abstract method not implemented.")
+
+    def get_metric_value(self):
+        raise NotImplementedError("Abstract method not implemented.")
+
+    def display_metric_value(self, epoch):
+        raise NotImplementedError("Abstract method not implemented.")
+
+    def reset_counter(self):
+        raise NotImplementedError("Abstract method not implemented.")
--- models/model_classifier.py
+++ models/model_classifier.py
@@ -1,44 +1,19 @@
-import importlib
 import torch
 import torch.nn as nn
+from . import Abstract_Model
 
-class CLS_Model(nn.Module):
+class CLS_Model(Abstract_Model):
     def __init__(self,
         backbone,
         backend,
-        created=False
+        backbone_created=False,
+        backend_created=False
     ):
-        super(CLS_Model, self).__init__()
-
-        # build modules
-        if not created:
-            self.backbone = CLS_Model.build_module(backbone)
-            self.backend = CLS_Model.build_module(backend)
-        else:
-            self.backbone = backbone
-            self.backend = backend
-
-        self.num_params_backbone = sum(param.numel() for param in self.backbone.parameters())
-        self.num_params_backend = sum(param.numel() for param in self.backend.parameters())
+        super(CLS_Model, self).__init__(backbone, backend, backbone_created, backend_created)
 
         # for test/val
         self.total = 0
         self.correct = 0
-
-    @staticmethod
-    def build_module(module_config):
-        class_name = module_config["name"].split('.')[-1]
-        module_name = module_config["name"][:(len(module_config["name"]))-len(class_name)-1]
-        
-        class_type = getattr(importlib.import_module(module_name), class_name)
-        obj = class_type(**module_config["config"])
-
-        return obj
-
-    def forward(self, x):
-        x = self.backbone(x)
-        out = self.backend(x)
-        return out
 
     def compute_metric(self, output, target):
         """Computes the accuracy over the k top predictions for the specified values of k"""
@@ -52,11 +27,6 @@ class CLS_Model(nn.Module):
 
     def display_metric_value(self, epoch):
         print(f'Epoch: {epoch}, validate accuracy: {self.get_metric_value()} %')
-
-    def get_num_params(self):
-        print(f"Backbone num of params: {self.num_params_backbone}")
-        print(f"Backend num of params: {self.num_params_backend}")
-        return self.num_params_backbone + self.num_params_backend
 
     def reset_counter(self):
         self.total = 0
