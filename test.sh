9da48a140b475f73bae181b4906d317de51bfc8f
shijianxu <13260906215@163.com>
2022-08-09 21:53:11 +0200

    n2n text overlay

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 python main.py \
     --mode test \
-    --config configs/noise2noise.py \
-    --ckpt ./logs/noise2noise/checkpoints/checkpoint_latest.pth
+    --config configs/noise2noise_text.py \
+    --ckpt ./logs/noise2noise_text/checkpoints/checkpoint_latest.pth
