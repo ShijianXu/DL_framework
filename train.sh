f9a741d89084b07b3e7d8085a5541721ecfef5dd
shijianxu <13260906215@163.com>
2022-08-10 22:53:44 +0200

    text train

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 python main.py \
     --mode train \
-    --config configs/noise2noise_gauss.py \
-    --log ./logs/noise2noise_gauss
+    --config configs/noise2noise_text.py \
+    --log ./logs/noise2noise_text
