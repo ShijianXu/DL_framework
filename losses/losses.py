b4b2ab6dc1bebff53ceff6f5a566c7873d979afe
shijianxu <13260906215@163.com>
2022-08-11 16:54:07 +0200

    fix VAE Loss error

--- losses/losses.py
+++ losses/losses.py
@@ -2,7 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def VAE_Loss(recons, input, mu, logvar, kld_weight=1):
+class VAE_Loss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, recons, input, mu, logvar, kld_weight=1):
+        recons_loss = F.mse_loss(recons, input)
+        kld_loss = torch.mean(-0.5 * torch.sum(1+logvar - mu**2 - logvar.exp(), dim=1), dim=0)
+
+        vae_loss = recons_loss + kld_weight*kld_loss
+        return {
+            "loss": vae_loss,
+            "recons_loss": recons_loss,
+            "KL-Divergence": -kld_loss
+        }
+
+
+def VAE_Loss_fn(recons, input, mu, logvar, kld_weight=1):
     recons_loss = F.mse_loss(recons, input)
     kld_loss = torch.mean(-0.5 * torch.sum(1+logvar - mu**2 - logvar.exp(), dim=1), dim=0)
 
