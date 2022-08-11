8993afad5547844439e8943a39fec57075114492
shijianxu <13260906215@163.com>
2022-08-11 16:19:19 +0200

    fix error

--- configs/cifar_cls_resnet.py
+++ configs/cifar_cls_resnet.py
@@ -24,12 +24,6 @@ model_config = {
 model = models.model_classifier.CLS_Model(**model_config)
 print(f"Total model parameters: {model.get_num_params()}")
 
-# Data part
-# transform=transforms.Compose([
-#     transforms.ToTensor(),
-#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-# ])
-
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
@@ -52,6 +46,15 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
+valid_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=transform_test)
+valid_dataloader = DataLoader(
+    valid_dataset, 
+    batch_size=4, 
+    shuffle=True, 
+    num_workers=2
+)
+print("Construct test dataset with {} samples".format(len(valid_dataset)))
+
 test_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=transform_test)
 test_dataloader = DataLoader(
     test_dataset, 
@@ -59,7 +62,6 @@ test_dataloader = DataLoader(
     shuffle=True, 
     num_workers=2
 )
-print("Construct test dataset with {} samples".format(len(test_dataset)))
 
 # Loss and training part
 learning_rate = 0.0001
