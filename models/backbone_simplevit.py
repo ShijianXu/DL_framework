2edeb6f622853308959dbc5abc231eb897f61297
shijianxu <13260906215@163.com>
2022-08-17 13:42:13 +0200

    add simplevit

--- models/backbone_simplevit.py
+++ models/backbone_simplevit.py
@@ -7,13 +7,79 @@ from einops.layers.torch import Rearrange
 
 from utils import pair, posemb_sincos_2d
 
+#=============================================================
+# def pair(t):
+#     return t if isinstance(t, tuple) else (t, t)
 
-class Transformer(nn.Module):
-    def __init__(self):
+# def posemb_sincos_2d(patches, temperature = 10000, dtype = torch.float32):
+#     _, h, w, dim, device, dtype = *patches.shape, patches.device, patches.dtype
+
+#     y, x = torch.meshgrid(torch.arange(h, device = device), torch.arange(w, device = device), indexing = 'ij')
+#     assert (dim % 4) == 0, 'feature dimension must be multiple of 4 for sincos emb'
+#     omega = torch.arange(dim // 4, device = device) / (dim // 4 - 1)
+#     omega = 1. / (temperature ** omega)
+
+#     y = y.flatten()[:, None] * omega[None, :]
+#     x = x.flatten()[:, None] * omega[None, :]
+
+#     pe = torch.cat((x.sin(), x.cos(), y.sin(), y.cos()), dim = 1)
+#     return pe.type(dtype)
+#=============================================================
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim):
         super().__init__()
+        self.net = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Linear(hidden_dim, dim),
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class Attention(nn.Module):
+    def __init__(self, dim, heads = 8, dim_head = 64):
+        super().__init__()
+        inner_dim = dim_head *  heads
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        self.norm = nn.LayerNorm(dim)
+
+        self.attend = nn.Softmax(dim = -1)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim, bias = False)
 
     def forward(self, x):
-        pass
+        x = self.norm(x)
+
+        qkv = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+
+        attn = self.attend(dots)
+
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                Attention(dim, heads = heads, dim_head = dim_head),
+                FeedForward(dim, mlp_dim)
+            ]))
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        return x
 
 
 class SimpleViT(nn.Module):
@@ -64,5 +130,19 @@ class SimpleViT(nn.Module):
         x = x.mean(dim = 1)
 
         x = self.to_latent(x)
-
         return self.linear_head(x)
+
+
+if __name__ == '__main__':
+    model = SimpleViT(
+        image_size = 256,
+        patch_size = 32,
+        num_classes = 1000,
+        dim = 1024,
+        depth = 6,
+        heads = 16,
+        mlp_dim = 2048
+    )
+    x = torch.rand(4, 3, 256, 256)
+    out = model(x)
+    print(out.shape)
