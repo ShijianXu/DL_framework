8e8d51fe841052972bf15e34c93b2950e11d3064
shijianxu <13260906215@163.com>
2022-08-19 21:06:33 +0200

    modify scripts

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 python main.py \
     --mode test \
     --config configs/cifar_cls_mlpmixer.py \
-    --ckpt ./logs/cifar_cls_mlpmixer_1_large_bs/checkpoints/checkpoint_latest.pth
+    --ckpt ./logs/cifar_cls_mlpmixer_large_bs/checkpoints/checkpoint_latest.pth
