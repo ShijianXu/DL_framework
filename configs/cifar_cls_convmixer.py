eeb1c5befbd5306e80cd146a52d2e107667ac077
shijianxu <13260906215@163.com>
2022-08-21 19:17:04 +0200

    convmixer cifar10 train

--- configs/cifar_cls_convmixer.py
+++ configs/cifar_cls_convmixer.py
@@ -39,9 +39,9 @@ transform_train = transforms.Compose([
 train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=transform_train)
 train_dataloader = DataLoader(
     train_dataset, 
-    batch_size=32, 
+    batch_size=16, 
     shuffle=True, 
-    num_workers=8
+    num_workers=4
 )
 print("Construct train dataset with {} samples".format(len(train_dataset)))
 
@@ -54,18 +54,18 @@ transform_test = transforms.Compose([
 valid_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=transform_test)
 valid_dataloader = DataLoader(
     valid_dataset, 
-    batch_size=32, 
+    batch_size=16, 
     shuffle=True, 
-    num_workers=8
+    num_workers=4
 )
 print("Construct test dataset with {} samples".format(len(valid_dataset)))
 
 test_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=transform_test)
 test_dataloader = DataLoader(
     test_dataset, 
-    batch_size=32, 
+    batch_size=16, 
     shuffle=True, 
-    num_workers=8
+    num_workers=4
 )
 test_require_gt = True
 
