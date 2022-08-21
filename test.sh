dcb1d431273f946d37456ab169032822cf17381f
shijianxu <13260906215@163.com>
2022-08-21 18:17:54 +0200

    convmixer cifar10 train

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 python main.py \
     --mode test \
-    --config configs/cifar_cls_mlpmixer.py \
-    --ckpt ./logs/cifar_cls_mlpmixer_large_bs/checkpoints/checkpoint_latest.pth
+    --config configs/cifar_cls_convmixer.py \
+    --ckpt ./logs/cifar_cls_convmixer/checkpoints/checkpoint_latest.pth
--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 python main.py \
     --mode train \
-    --config configs/cifar_cls_mlpmixer.py \
-    --log ./logs/cifar_cls_mlpmixer_large_bs
+    --config configs/cifar_cls_convmixer.py \
+    --log ./logs/cifar_cls_convmixer
